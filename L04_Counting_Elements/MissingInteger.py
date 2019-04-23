9ba7ca54a32c9210e4098cc83e14e281b6a14f62
pi_chiu <rtchiou@gmail.com>
2019-04-23 20:46:33 +0800

    Create MissingInteger.py
    https://app.codility.com/programmers/lessons/4-counting_elements/missing_integer/

---
+++ L04_Counting_Elements/MissingInteger.py
@@ -0,0 +1,14 @@
+﻿# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+# https://app.codility.com/demo/results/training9QTF9R-SHK/
+
+def solution(A):
+    # write your code in Python 3.6
+    nums = sorted([num for num in A if num > 0])
+    prev = 0
+    for num in nums:
+        if num - prev > 1:
+            return prev + 1
+        prev = num
+    return prev + 1
